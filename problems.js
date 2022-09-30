f1c30cf07b755c0f32760f4e0494ab1b8034e929
Natalie <hairnatalie7@gmail.com>
2022-09-30 11:25:49 -0600

    first commit

---
+++ problems.js
@@ -0,0 +1,16 @@
+let nums = [2, 4, 8, 7, 0, 0, 2]
+
+function addToZero (nums) {
+    for (let i=0; i<nums.length; i++)
+}
+
+
+
+function hasUniqueChars (str) {
+    let uniqueChars = new Set ([])
+    for (let i=0; i<str.length; i++){
+        uniqueCHars.add(word[i])
+    }
+    return uniqueChars.size===word.length
+}
+
